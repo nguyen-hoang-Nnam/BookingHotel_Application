fac12aff6f15cc4423e4c6be55f1ad8f8cfcedd2
Nam Hoàng <h0angnnam1123@gmail.com>
2024-10-27 21:07:43 +0700

    update

--- BookingHotel_Application.Model/Models/DTO/Countries/CountriesDTO.cs
+++ BookingHotel_Application.Model/Models/DTO/Countries/CountriesDTO.cs
@@ -8,6 +8,7 @@ namespace BookingHotel_Application.Model.Models.DTO.Countries
 {
     public class CountriesDTO
     {
+        public int countryId { get; set; }
         public string countryName { get; set; }
     }
 }
